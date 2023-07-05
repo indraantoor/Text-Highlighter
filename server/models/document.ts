84e760638b54902eb94aaa03b6e3dc1b251b3e60
indraantoor <indraantoor@gmail.com>
2023-07-05 19:38:23 +0530

    model: add validations to document model
    Signed-off-by: indraantoor <indraantoor@gmail.com>

--- server/models/document.ts
+++ server/models/document.ts
@@ -1,17 +1,29 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface IDocument extends Document {
-  title: string;
+  name: string;
   content: string;
   author: string;
   date: Date;
 }
 
 const documentSchema = new Schema<IDocument>({
-  title: String,
-  content: String,
-  author: String,
-  date: Date,
+  name: {
+    type: String,
+    required: true,
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  author: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
 export default mongoose.model<IDocument>("Document", documentSchema);
